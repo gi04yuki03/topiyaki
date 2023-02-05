9f3298303613162011136071675cbf0bd2143121
gi04yuki03 <spica_sigsig@yahoo.co.jp>
2023-02-05 17:32:04 +0900

    <recipes>newページ作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,19 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.save
+    redirect_to recipe_path(recipe.id)
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :image)
+  end
 end
