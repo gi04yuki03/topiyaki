d185b763800f47f590ab4a8ef02e2521c769c587
gi04yuki03 <spica_sigsig@yahoo.co.jp>
2023-02-06 19:18:49 +0900

    <recipes>user_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_06_085115) do
+ActiveRecord::Schema.define(version: 2023_02_06_093205) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_02_06_085115) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
