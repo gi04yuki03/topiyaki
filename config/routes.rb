416af91893ee1908226d847cd11461ba5158fc6f
gi04yuki03 <spica_sigsig@yahoo.co.jp>
2023-02-06 18:45:35 +0900

    <users>show,editページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes
+  resources :users, only:[:show, :edit]
 end
