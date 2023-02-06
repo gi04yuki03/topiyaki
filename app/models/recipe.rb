0b1c8d5b778f097259f04dec9a457b4b418834d5
gi04yuki03 <spica_sigsig@yahoo.co.jp>
2023-02-06 19:36:08 +0900

    <favorites>モデル、コントローラー作成

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ApplicationRecord
   attachment :image
 
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes
   resources :users, only:[:show, :edit]
+  resource :favorites, only:[:create, :destroy]
 end
