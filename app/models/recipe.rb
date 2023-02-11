e92a9c483d37d521c1c4086db1ea614545414724
gi04yuki03 <spica_sigsig@yahoo.co.jp>
2023-02-11 20:15:59 +0900

    <recipes,users>バリデーション設定

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,10 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
 
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, presence: true, length: { maximum: 100 }
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
