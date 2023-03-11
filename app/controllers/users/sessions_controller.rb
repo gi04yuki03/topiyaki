0db47a1ced703cf302b4628acdba208075e22cfd
gi04yuki03 <spica_sigsig@yahoo.co.jp>
2023-03-11 21:42:50 +0900

    ログイン時エラーメッセージを表示

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,9 +1,45 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
   def guest_sign_in
     user = User.guest
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  private
+
+  def auth_options
+    { scope: resource_name, recall: "#{controller_path}#new" }
+  end
+
+  def require_no_authentication
+    assert_is_devise_resource!
+    return unless is_navigational_format?
+    no_input = devise_mapping.no_input_strategies
+
+    authenticated = if no_input.present?
+      args = no_input.dup.push scope: resource_name
+      warden.authenticate?(*args)
+    else
+      warden.authenticated?(resource_name)
+    end
+
+    if authenticated && resource = warden.user(resource_name)
+      flash[:alert] = "すでにログインしています。"
+      redirect_to after_sign_in_path_for(resource)
+    end
+  end
+
+  def translation_scope
+    'devise.sessions'
   end
 end
